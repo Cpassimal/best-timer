21367047a65b5f1f69f6757a33850a475667881f
cpassimal <goudin.romain.10@gmail.com>
2019-04-16 22:29:54 +0200

    refacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChronometerComponent } from './chronometer/chronometer.component';
+import { CountdownComponent } from './countdown/countdown.component';
 
 import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ChronometerComponent,
+    CountdownComponent,
   ],
   imports: [
     BrowserModule,
     PipesModule,
+    AppRoutingModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
